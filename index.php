3693a3f937bb00b1016e6d4baab989c36118aecf
maximugisha <maximugisha@gmail.com>
2020-04-10 14:44:51 +0300

    chenged to php for delployment

---
+++ index.php
@@ -0,0 +1,297 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Apointed Harvesters Miracle church</title>
+
+  <!-- Font Awesome Icons -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet">
+  <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
+
+  <!-- Plugin CSS -->
+  <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+  <!-- Theme CSS - Includes Bootstrap -->
+  <link href="css/creative.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">A.H.M.C</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto my-2 my-lg-0">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#services">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Masthead -->
+  <header class="masthead">
+    <div class="container h-100">
+      <div class="row h-100 align-items-center justify-content-center text-center">
+        <div class="col-lg-10 align-self-end">
+          <h1 class="text-uppercase text-white font-weight-bold">Appointed Harvesters Miracle Church</h1>
+          <hr class="divider my-4">
+        </div>
+        <div class="col-lg-8 align-self-baseline">
+          <p class="text-white-75 font-weight-light mb-5">At Appointed harvesters Miracle Church, - Everybody is someone. We welcome you all to come and worship and praise the Lord, for this we believe is the only thing we can do for our Lord. The sovereign Lord.</p>
+          <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Find Out More</a>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- About Section -->
+  <section class="page-section bg-primary" id="about">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8 text-center">
+          <h2 class="text-white mt-0">About Appointed Harvesters Miracle church!</h2>
+          <hr class="divider light my-4">
+          <p class="text-white-50 mb-4">we are a ministry that is God inspired! A ministry with such a humble beginning, a steady rise by the power of the Holy Spirit, and with a clear vision of where God is taking us.
+          We are led by the vision Bearer, The mighty man ov Valor yet humble and meek. withought forgetting the love and passion he has for the body of Christ, and the extension of love to the despised.
+          To mention but a few, he is a both local and internation preacher, An apostle by calling, an Artist by Inspiration and a life changing mindset he has, has really transformed the lives of many!
+          We can take weeks or even months to talk about the vison bearer, but lets preach. -  it is the ultimate calling of this ministry. - The man is Apostle Alpha Kimera.
+          </p>
+          <a class="btn btn-light btn-xl js-scroll-trigger" href="#services">Our services!</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Services Section -->
+  <section class="page-section" id="services">
+    <div class="container">
+      <h2 class="text-center mt-0">Services At Appointed Harvesters Miracle church</h2>
+      <p class="text-black-50 mb-4">By services, we supercede the normal definition of services in the usual church seting, we have a wide range of services the we minister to the body of Christ that include and not limited to; 
+      </p>
+      <hr class="divider my-4">
+      <div class="row">
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-gem text-primary mb-4"></i>
+            <h3 class="h4 mb-2">Bible Study Themes</h3>
+            <p class="text-muted mb-0">We have a full course of bible study!
+              it is aimed at upbringing a complete and whole Ministers of God in this and the generations to come.
+              we carry out bible study every saturday evening before Choir pracrices, and every sunday, the first Alter service is aimed for Ministers and servers as a continuation of the bible study as it builds up from the previous studies,  accompanied by Prayer led by the main Pastor, to empower the ministers with the impartation of the Holy spirit to continue with the rest of the days' services in power and might.
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-laptop-code text-primary mb-4"></i>
+            <h3 class="h4 mb-2">Guidance & Counselling </h3>
+            <p class="text-muted mb-0">The Apostle himself, owing to the fact that he is a man of passion from the branded least to the branded mighty, he spares time for everyone in every capacity to meet them one on one, to listen and talk in their life.
+              This is a calling he recieved naturally, he wont talk in your life and stay the same, the manifestation of the Lord will be called upon your life and by the power of the Holy Spirit, you shall surely receive.
+              As if not enough, we have a certified Cousellor, from school, as the Second pastor. We call her Auntie Jane, she will talk to you and yuour soul will be at peace again. she is a GodSend. In all your life matters, God has put a word for you on her heart.
+              Meeting with the Pastor is by appointment. Check our contact section. God bless you.
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-globe text-primary mb-4"></i>
+            <h3 class="h4 mb-2">Weekly services</h3>
+            <p class="text-muted mb-0">In the understandable Service language, these are our weekly services at church!</p>
+            <ul>
+              <li>Wednesday - Bible Study & Evaluation Service - 5pm - 7pm</li>
+              <li>Friday - Healing Service - 5pm - 8pm</li>
+              <li>Overnight every last Friday of the month - </li>
+              <h2>Sunday Services </h2>
+              <li>Morning - Ministers Service - English </li>
+              <li>Main - Main Service - English / Luganda </li>
+              <li>Evening - Deliverance Service - The Holy Spirit inspired language </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-heart text-primary mb-4"></i>
+            <h3 class="h4 mb-2">Media </h3>
+            <p class="text-muted mb-0">The church, Appointed harvesters Miracle Church, owns a variety of media avenues with an unlimited scope and yet still growing.
+              The following is a list of some of the avenues of media we have.
+              the have one major aim, - Promoting Gosple reach to the boundaries beyond some human reach. We train our ministers using such media to also uplift their skills that they can use to grow themselves and as well advance in the ministry especially in doing the work of the Lord.
+              <ul>
+                <li>Audio Studio</li>
+                <li>Video Studio</li>
+                <li>Photography Studio</li>
+                <li> Public Address Sytem</li>
+              </ul>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Portfolio Section -->
+  <section id="portfolio">
+    <h2 class="text-black mt-0">Church Projects</h2>
+          <hr class="divider light my-4">
+    <div class="container-fluid p-0">
+      <div class="row no-gutters">
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/1.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/1.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Mission outreach
+              </div>
+              <div class="project-name">
+                Annual End of Year Passover
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/2.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/2.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Help the Needy Outreach
+              </div>
+              <div class="project-name">
+                Kayunga Save the needy project
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/3.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/3.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Talent Search
+              </div>
+              <div class="project-name">
+                QuickTouch records Talent Search
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/4.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/4.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Annual Football Competions
+              </div>
+              <div class="project-name">
+                Masanafu  Football Club
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/5.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/5.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Annual Women's COnference
+              </div>
+              <div class="project-name">
+                Women Poverty Alleviation Project
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/6.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/6.jpg" alt="">
+            <div class="portfolio-box-caption p-3">
+              <div class="project-category text-white-50">
+                Church Agriculture
+              </div>
+              <div class="project-name">
+                The piggery Project
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Call to Action Section -->
+  <section class="page-section bg-dark text-white">
+    <div class="container text-center">
+      <h2 class="mb-4">More About the Church Projects</h2>
+      <a class="btn btn-light btn-xl" href="#">View Now!</a>
+    </div>
+  </section>
+
+  <!-- Contact Section -->
+  <section class="page-section" id="contact">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8 text-center">
+          <h2 class="mt-0">Let's Get In Touch!</h2>
+          <hr class="divider my-4">
+          <p class="text-muted mb-5">For any inquiries, Prayer Requests, or any other information, Kindly reach us on</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
+          <i class="fas fa-phone fa-3x mb-3 text-muted"></i>
+          <div>+256 754 369-837</div>
+          <div>+256 775 097-505</div>
+          <div>+256 754 851-718</div>
+        </div>
+        <div class="col-lg-4 mr-auto text-center">
+          <i class="fas fa-envelope fa-3x mb-3 text-muted"></i>
+          <!-- Make sure to change the email address in anchor text AND the link below! -->
+          <a class="d-block" href="mailto:ahmcchurch@gmail.com">ahmcchurch@gmail.com</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="bg-light py-5">
+    <div class="container">
+      <div class="small text-center text-muted">Copyright &copy; 2020 - Appointed Harvesters Miracle Church</div>
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/creative.min.js"></script>
+
+</body>
+
+</html>
